1aa341a79dd88768011a1ba503c897ef4eeac318
anastasia.markina <ahmarkina@gmail.com>
2017-01-22 22:43:10 +0100

    temp result. Added the slider.

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,11 @@
  * Created by anastasia.markina on 2016-12-10.
  */
 $(document).ready(function(){
-    $('.WrapSlider').slick();
+    $('.WrapSlider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
 });
