3f20d915b9256c530778dbb166c9ce1ab82fdc8a
anastasia.markina <ahmarkina@gmail.com>
2017-01-23 23:47:36 +0100

    temp result. Fixed block 'Approach'.

--- build/js/script.js
+++ build/js/script.js
@@ -2,11 +2,32 @@
  * Created by anastasia.markina on 2016-12-10.
  */
 $(document).ready(function(){
-    $('.WrapSlider').slick({
+    /*$('.WrapSliderLeftSide').slick({
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1
+    });*/
+
+    $('.WrapSliderLeft').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        fade: false,
+        speed: 500,
+        infinite: true,
+        asNavFor: '.WrapSliderRight'
+    });
+
+    $('.WrapSliderRight').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.WrapSliderLeft',
+        dots: false,
+        fade: true,
+        infinity: true,
+        focusOnSelect: true
     });
 });
